128c61f3b90248ca5cf0c4e4e42b3b44925e1331
giamazonas <skibinskirenee@gmail.com>
2022-02-27 12:41:00 -0500

    add links

--- routes/flights.js
+++ routes/flights.js
@@ -3,12 +3,12 @@ const router = Router()
 import * as flightsCtrl from '../controllers/flights.js'
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.render('respond with a resource')
-})
+// router.get('/', function(req, res) {
+//   res.render()
+// })
 
 // GET /flights/new
-router.get('/', flightsCtrl.index)
+router.get('/index', flightsCtrl.index)
 router.get('/new', flightsCtrl.new)
 router.post('/', flightsCtrl.create)
 
